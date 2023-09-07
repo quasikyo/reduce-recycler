cc0c017f9ee44be2292f823e8db4f89a7bc45a9a
Michael Rehman <michael@michaelrehman.com>
2023-09-07 00:38:21 -0400

    Reset recycler cooldown successfully

--- ReduceRecycler/ReduceRecycler.cs
+++ ReduceRecycler/ReduceRecycler.cs
@@ -1,8 +1,13 @@
-﻿using BepInEx;
+﻿using System.Collections;
+
+using UnityEngine;
+
+using BepInEx;
+using RoR2;
 
 namespace ReduceRecycler {
+	// [BepInDependency("com.rune580.riskofoptions")]
 	[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-	[BepInDependency("com.rune580.riskofoptions")]
 	public class ReduceRecycler : BaseUnityPlugin {
 
 		public const string PluginGUID = PluginAuthor + "." + PluginName;
@@ -10,11 +15,22 @@ namespace ReduceRecycler {
 		public const string PluginName = "ReduceRecycler";
 		public const string PluginVersion = "0.1.0";
 
-
 		public void Awake() {
 			Log.Init(Logger);
 			Log.Info($"Performing setup for {nameof(ReduceRecycler)}");
+
+			On.RoR2.EquipmentSlot.FireRecycle += (On.RoR2.EquipmentSlot.orig_FireRecycle orig, EquipmentSlot self) => {
+				bool result = orig(self);
+				// Game state doesn't update quick enough OR until after this hook is done running (weird)
+				StartCoroutine(ResetRecyclerCooldown(self));
+				return result;
+			};
+
 		}
 
+		private IEnumerator ResetRecyclerCooldown(EquipmentSlot equipment) {
+			yield return new WaitForSeconds(0.5f);
+			equipment.characterBody.inventory.DeductActiveEquipmentCooldown(equipment.cooldownTimer);
+		}
 	}
 }
